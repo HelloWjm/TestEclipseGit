b8af1554d6ce2e9ad49e3388c5d28e3f59409fc0
HelloWjm <39674575+HelloWjm@users.noreply.github.com>
2018-05-28 15:19:49 +0800

    Test push success the second time!
    Signed-off-by: HelloWjm <39674575+HelloWjm@users.noreply.github.com>

--- src/cn/wjm/testOne/TestOne.java
+++ src/cn/wjm/testOne/TestOne.java
@@ -5,7 +5,7 @@ public class TestOne {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!"); 
 		System.out.println("Test push success!");
-
+		System.out.println("Test push success the second time!");
 	}
 
 }
